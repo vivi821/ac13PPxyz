4abbe39211bdcceef6024832b1049be9d97f1d31
maximuschang <gn00982591@msn.com>
2015-04-21 22:47:19 +0800

    2015042101

--- PortalLogin.aspx.cs
+++ PortalLogin.aspx.cs
@@ -7,8 +7,27 @@ using System.Web.UI.WebControls;
 
 public partial class PortalLogin : System.Web.UI.Page
 {
+    protected string ecode = "";
+
     protected void Page_Load(object sender, EventArgs e)
     {
+        //Request.Form["hidecode"]
+        if (!IsPostBack)
+        {
+            using (var db = new DBDataContext())
+            {
+                switch (Request.Form["hidfunc"])
+                {
+                    case "s":
+                        //var login=db.ESP_Login(Request.Form["uid"],Request.Form["uid"],)
+                        break;
+                    default:
+                        var code = db.ESP_eCode().FirstOrDefault();
+                        ecode = code == null ? "" : code.CodeSubTypeDesp;
+                        break;
+                }
+            }
+        }
 
     }
 }
